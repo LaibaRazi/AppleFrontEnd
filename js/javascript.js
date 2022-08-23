4fb69a5e4f2d3454021454aa7fba3b3b2c579f5c
LaibaRazi <khanlayba72@gmail.com>
2022-08-23 20:58:45 +0500

    Front-End

---
+++ js/javascript.js
@@ -0,0 +1,9 @@
+
+function fixedNav(){
+    document.getElementsByTagName("nav").style.position="sticky";
+}
+document.getElementsByTagName("nAct").addEventListener("click",open())
+function open() {
+document.getElementById("nAct").classList.remove("nAct");
+    
+}
